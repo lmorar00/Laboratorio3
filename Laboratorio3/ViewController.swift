8b23730eb0281dd9c6568aceb5e156f5f98561c2
Luis Guillermo Mora Rivas <lmorar00@hotmail.com>
2021-08-28 12:28:55 -0600

    Initial Commit

---
+++ Laboratorio3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Laboratorio3
+//
+//  Created by Luis Mora Rivas on 28/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
